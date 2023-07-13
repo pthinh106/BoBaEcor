cd1916ad251fa79e2b709071fad76421d7ee5301
Nguyen Phuoc Thinh <4601104176@student.hcmue.edu.vn>
2023-07-13 11:07:33 +0700

    update

--- src/main/java/TMDTBoBa/BoBaEcor/Controller/Admin/AdminController.java
+++ src/main/java/TMDTBoBa/BoBaEcor/Controller/Admin/AdminController.java
@@ -3,8 +3,6 @@ package TMDTBoBa.BoBaEcor.Controller.Admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 @Controller
 @RequestMapping(path = "/admin")
@@ -13,5 +11,10 @@ public class AdminController {
     @GetMapping("/login")
     public String login(){
         return "admin/login";
+    }
+
+    @GetMapping("/category")
+    public String category(){
+        return "admin/Store/manager_category";
     }
 }
