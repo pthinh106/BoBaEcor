c703c4e02b5829c610e753245fe8634f96ab6399
huy9633 <lamchanhuy9633@gmail.com>
2023-08-09 19:54:38 +0700

    Update_RSS

--- src/main/java/TMDTBoBa/BoBaEcor/Controller/Home/HomeController.java
+++ src/main/java/TMDTBoBa/BoBaEcor/Controller/Home/HomeController.java
@@ -1,14 +1,22 @@
 package TMDTBoBa.BoBaEcor.Controller.Home;
 
+import TMDTBoBa.BoBaEcor.Models.BlogCustom.RSSItem;
+import com.rometools.rome.feed.synd.SyndEntry;
+import TMDTBoBa.BoBaEcor.Models.BlogCustom.Kenh14RSSReader;
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.model.IModel;
 
+import java.util.List;
 @Controller
 @RequestMapping(path = "")
 public class HomeController {
+
     @GetMapping("/")
     public String index(Model model, HttpServletRequest request){
 
@@ -36,9 +44,20 @@ public class HomeController {
     public String checkOut(){
         return "home/checkout";
     }
+
+    @Autowired
+    private Kenh14RSSReader rssReader;
     @GetMapping("/tin-tuc")
-    public String blog(){
-        return "home/blog";
+    public String blog(Model model){
+        String rssFeedUrl = "https://vnexpress.net/rss/giai-tri.rss";
+
+        List<RSSItem> items = rssReader.readRSSFeed(rssFeedUrl);
+        if (items != null) {
+            model.addAttribute("items", items);
+        } else {
+            model.addAttribute("error", "Failed to read RSS feed.");
+        }
+        return "home/rss";
     }
 
 }
