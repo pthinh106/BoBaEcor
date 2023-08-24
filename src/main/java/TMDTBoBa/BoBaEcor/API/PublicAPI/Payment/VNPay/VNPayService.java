7885c3e1b19faf6beb970fc5d22dda439c59a70c
Nguyen Phuoc Thinh <4601104176@student.hcmue.edu.vn>
2023-08-24 10:26:45 +0700

    fix add product

--- src/main/java/TMDTBoBa/BoBaEcor/API/PublicAPI/Payment/VNPay/VNPayService.java
+++ src/main/java/TMDTBoBa/BoBaEcor/API/PublicAPI/Payment/VNPay/VNPayService.java
@@ -41,7 +41,7 @@ public class VNPayService {
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
-        cld.add(Calendar.MINUTE, 15);
+        cld.add(Calendar.HOUR, 24);
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 
